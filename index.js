aa9bba83c5446a169738054d376f1db28b7072b9
Prerna <iprernasingh4@gmail.com>
2023-04-28 22:16:46 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,88 @@
+//Http module
+
+const http = require('http')
+
+const fs = require('fs')
+
+const path = require('path')
+const app = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    })
+   /* if(req.url == '/'){
+        fs.readFile(path.join (__dirname, 'public', 'index.html'), (err, content)=>{
+
+            if(err){
+                throw err
+            }
+    
+            res.end(content)
+        })
+    } else if (req.url == '/about'){
+        fs.readFile(path.join (__dirname, 'public', 'about.html'), (err, content)=>{
+
+            if(err){
+                throw err
+            }
+    
+            res.end(content)
+        })
+    }*/
+   
+     let filePath = path.join(__dirname, 'public', req.url == '/' ? 'index.html' : req.url) 
+
+     let ContentType = 'text/html'
+
+     let ext = path.extname(filePath)
+     if(!ext){
+        filePath += '.html'
+     }
+
+     switch(ext){
+        case '.css':
+           ContentType = 'text/css'
+           break
+        case '.js':
+            ContentType = 'text/javascript'
+            break
+        default:
+            ContentType = 'text/html'
+    }
+
+     fs.readFile(filePath, (err, content) => {
+        if(err) {
+            fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) => {
+
+                if(err) {
+
+                    res.writeHead(500)
+                    res.end('Error!!!')
+
+                } else {
+                    res.writeHead(404,{
+                        'Content-Type': ContentType
+                    })
+                    res.end(data)
+                }
+               
+            })
+        } else {
+          res.writeHead(200, {
+            'Content-Type': ContentType
+          })
+
+          res.end(content)
+        }
+        
+         /*if(err){
+            res.writeHead(500)
+            res.end('Error!!!')
+         }*/
+     })
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
